288803981d84302f683af242b7f8354db26105bb
Muxian Wu <mw@tectusdreamlab.com>
2018-03-05 14:27:22 +0800

    made public url and redis url optional, default to local

--- configs.js
+++ configs.js
@@ -4,11 +4,11 @@ module.exports = {
 
   // URL of the server where this app is deployed to (relevant for callbacks)
   // note: localhost or 127.0.0.1 will not work with callbacks from 3d.io -> use ngrok for local testing
-  url: process.env.PUBLIC_SERVER_URL,
+  url: process.env.PUBLIC_SERVER_URL || '127.0.0.1',
 
   // website domains which will be allowed to use this API (CORS)
   corsAllowedDomains: [
-    process.env.PUBLIC_SERVER_URL.replace('https://','').replace('http://',''),
+    // process.env.PUBLIC_SERVER_URL.replace('https://','').replace('http://',''),
     'localhost',
     'localhost:3000'
   ],
@@ -29,7 +29,7 @@ module.exports = {
   // get your 3d.io secret API key from: https://3d.io/dev-dashboard/
   secretApiKey: process.env.IO3D_SECRET_API_KEY,
 
-  redis_url: process.env.REDIS_URL,
+  redis_url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
 
   // email (using nodemailer module and sendGrid API)
   // feel free to replace this with configs specific to your email service of choise
